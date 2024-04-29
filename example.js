3102449b802ed0c330609c204a238dae068cafd6
dagmawig <dgebreselasse@gmail.com>
2024-04-28 21:23:36 -0500

    feat: parse all headers into JSON file and log output

---
+++ example.js
@@ -0,0 +1,363 @@
+const htmlString = `
+<div class="title" id="title-2">
+	<h1>Title 2—Grants and Agreements</h1>
+
+	<div class="subtitle" id="subtitle-A">
+		<h2>Subtitle A—Office of Management and Budget Guidance for Grants and Agreements
+		</h2>
+
+		<div class="part" id="part-1">
+			<h1
+				data-hierarchy-metadata="{&quot;path&quot;:&quot;/on/2024-03-01/title-2/part-1&quot;,&quot;citation&quot;:&quot;2 CFR Part 1&quot;}">
+				PART 1—ABOUT <a href="/on/2024-03-01/title-2" class="cfr external">TITLE 2 OF THE CODE OF FEDERAL
+					REGULATIONS</a> AND SUBTITLE A
+			</h1>
+
+			<div class="authority">
+				<h4 class="inline-header">Authority:</h4>
+				<p class="inline-paragraph"><a href="https://www.govinfo.gov/link/uscode/31/503" class="usc external"
+						target="_blank" rel="noopener noreferrer">31 U.S.C. 503</a>; <a
+						href="https://www.govinfo.gov/link/uscode/31/1111" class="usc external" target="_blank"
+						rel="noopener noreferrer">31 U.S.C. 1111</a>; <a
+						href="https://www.govinfo.gov/link/uscode/41/405" class="usc external" target="_blank"
+						rel="noopener noreferrer">41 U.S.C. 405</a>; Reorganization Plan No. 2 of 1970; <a
+						href="https://www.federalregister.gov/executive-order/11541" class="eo external" target="_blank"
+						rel="noopener noreferrer">E.O. 11541</a>, <a
+						href="https://www.federalregister.gov/citation/35-FR-10737" class="fr-reference"
+						data-reference="35 FR 10737">35 FR 10737</a>, <a href="/on/2024-03-01/title-3"
+						class="cfr external">3 CFR</a>, 1966-1970, p. 939.
+				</p>
+			</div>
+
+			<div class="source">
+				<h4 class="inline-header">Source:</h4>
+				<p class="inline-paragraph"><a href="https://www.federalregister.gov/citation/69-FR-26280"
+						class="fr-reference" data-reference="69 FR 26280">69 FR 26280</a>, May 11, 2004, unless
+					otherwise noted.
+				</p>
+			</div>
+
+			<div class="subpart" id="subpart-A">
+				<h2
+					data-hierarchy-metadata="{&quot;path&quot;:&quot;/on/2024-03-01/title-2/part-1/subpart-A&quot;,&quot;citation&quot;:&quot;2 CFR Part 1 Subpart A&quot;}">
+					Subpart A—Introduction to <a href="/on/2024-03-01/title-2" class="cfr external">Title 2 of the
+						CFR</a></h2>
+				<div class="section" id="1.100">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.100","citation":"2 CFR 1.100"}'>
+						§ 1.100 Content of this title.</h4>
+					<p>This title contains— </p>
+					<div id="p-1.100(a)">
+						<p class="indent-1" data-title="1.100(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							Office of Management and Budget (OMB) guidance to Federal agencies on government-wide
+							policies and procedures for the award and administration of grants and agreements; and </p>
+					</div>
+					<div id="p-1.100(b)">
+						<p class="indent-1" data-title="1.100(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							Federal agency regulations implementing that OMB guidance. </p>
+					</div>
+				</div>
+
+				<div class="section" id="1.105">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.105","citation":"2 CFR 1.105"}'>
+						§ 1.105 Organization and subtitle content.</h4>
+					<div id="p-1.105(a)">
+						<p class="indent-1" data-title="1.105(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							This title is organized into two subtitles. </p>
+					</div>
+					<div id="p-1.105(b)">
+						<p class="indent-1" data-title="1.105(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							The OMB guidance described in <a href="/on/2024-03-01/title-2/section-1.100#p-1.100(a)"
+								class="cfr external">§ 1.100(a)</a> is published in subtitle A. Publication of the OMB
+							guidance in the CFR does not change its nature—it is guidance and not regulation. </p>
+					</div>
+					<div id="p-1.105(c)">
+						<p class="indent-1" data-title="1.105(c)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>c<span class="paren">)</span></span>
+							Each Federal agency that publishes regulations implementing the OMB guidance has a chapter
+							in subtitle B in which it issues those regulations. The Federal agency regulations in
+							subtitle B differ in nature from the OMB guidance in subtitle A because the OMB guidance is
+							not regulatory (Federal agency regulations in subtitle B may give regulatory effect to the
+							OMB guidance, to the extent that the agency regulations require compliance with all or
+							portions of the guidance). </p>
+					</div>
+				</div>
+
+				<div class="section" id="1.110">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.110","citation":"2 CFR 1.110"}'>
+						§ 1.110 Issuing authorities.</h4>
+					<p>OMB issues this subtitle. Each Federal agency that has a chapter in <a
+							href="/on/2024-03-01/title-2/subtitle-B" class="cfr external">subtitle B of this title</a>
+						issues that chapter. </p>
+				</div>
+
+			</div>
+
+			<div class="subpart" id="subpart-B">
+				<h2
+					data-hierarchy-metadata="{&quot;path&quot;:&quot;/on/2024-03-01/title-2/part-1/subpart-B&quot;,&quot;citation&quot;:&quot;2 CFR Part 1 Subpart B&quot;}">
+					Subpart B—Introduction to Subtitle A</h2>
+				<div class="section" id="1.200">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.200","citation":"2 CFR 1.200"}'>
+						§ 1.200 Purpose of chapters I and II.</h4>
+					<div id="p-1.200(a)">
+						<p class="indent-1" data-title="1.200(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							Chapters I and II of subtitle A provide OMB guidance to Federal agencies that helps ensure
+							consistent and uniform government-wide policies and procedures for management of the
+							agencies' grants and agreements. </p>
+					</div>
+					<div id="p-1.200(b)">
+						<p class="indent-1" data-title="1.200(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							There are two chapters for publication of the guidance because portions of it may be revised
+							as a result of ongoing efforts to streamline and simplify requirements for the award and
+							administration of grants and other financial assistance (and thereby implement the Federal
+							Financial Assistance Management Improvement Act of 1999, <a
+								href="https://www.govinfo.gov/link/plaw/106/public/107" class="publ external"
+								target="_blank" rel="noopener noreferrer">Pub. L. 106-107</a>). </p>
+					</div>
+					<div id="p-1.200(c)">
+						<p class="indent-1" data-title="1.200(c)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>c<span class="paren">)</span></span>
+							The OMB guidance in its initial form—before completion of revisions described in <a
+								href="/on/2024-03-01/title-2/section-1.200#p-1.200(b)" class="cfr external">paragraph
+								(b)</a> of this section—is published in chapter II of this subtitle. When revisions to a
+							part of the guidance are finalized, that part is published in chapter I and removed from
+							chapter II. </p>
+					</div>
+				</div>
+
+				<div class="section" id="1.205">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.205","citation":"2 CFR 1.205"}'>
+						§ 1.205 Applicability to grants and other funding instruments.</h4>
+					<p>The types of instruments that are subject to the guidance in this subtitle vary from one portion
+						of the guidance to another (note that each part identifies the types of instruments to which it
+						applies). All portions of the guidance apply to grants and cooperative agreements, some portions
+						also apply to other types of financial assistance or nonprocurement instruments, and some
+						portions also apply to procurement contracts. For example, the: </p>
+					<div id="p-1.205(a)">
+						<p class="indent-1" data-title="1.205(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							Guidance on debarment and suspension in part 180 of this subtitle applies broadly to all
+							financial assistance and other nonprocurement transactions, and not just to grants and
+							cooperative agreements. </p>
+					</div>
+					<div id="p-1.205(b)">
+						<p class="indent-1" data-title="1.205(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							Cost principles in parts 220, 225 and 230 of this subtitle apply to procurement contracts,
+							as well as to financial assistance, although those principles are implemented for
+							procurement contracts through the Federal Acquisition Regulation in <a
+								href="/on/2024-03-01/title-48" class="cfr external">title 48 of the CFR</a>, rather than
+							through Federal agency regulations on grants and agreements in this title. </p>
+					</div>
+					<p class="citation">[<a href="https://www.federalregister.gov/citation/70-FR-51863"
+							class="fr-reference" data-reference="70 FR 51863">70 FR 51863</a>, Aug. 31, 2005]
+					</p>
+				</div>
+
+				<div class="section" id="1.210">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.210","citation":"2 CFR 1.210"}'>
+						§ 1.210 Applicability to Federal agencies and others.</h4>
+					<div id="p-1.210(a)">
+						<p class="indent-1" data-title="1.210(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							This subtitle contains guidance that directly applies only to Federal agencies. </p>
+					</div>
+					<div id="p-1.210(b)">
+						<p class="indent-1" data-title="1.210(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							The guidance in this subtitle may affect others through each Federal agency's implementation
+							of the guidance, portions of which may apply to— </p>
+						<div id="p-1.210(b)(1)">
+							<p class="indent-2" data-title="1.210(b)(1)">
+								<span class="paragraph-hierarchy"><span class="paren">(</span>1<span class="paren">)</span></span>
+								The agency's awarding or administering officials; </p>
+						</div>
+						<div id="p-1.210(b)(2)">
+							<p class="indent-2" data-title="1.210(b)(2)">
+								<span class="paragraph-hierarchy"><span class="paren">(</span>2<span class="paren">)</span></span>
+								Non-Federal entities that receive or apply for the agency's grants or agreements or
+								receive subawards under those grants or agreements; or </p>
+						</div>
+						<div id="p-1.210(b)(3)">
+							<p class="indent-2" data-title="1.210(b)(3)">
+								<span class="paragraph-hierarchy"><span class="paren">(</span>3<span class="paren">)</span></span>
+								Any other entities involved in agency transactions subject to the guidance in this
+								chapter. </p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section" id="1.215">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.215","citation":"2 CFR 1.215"}'>
+						§ 1.215 Relationship to previous issuances.</h4>
+					<p>Although some of the guidance was organized differently within OMB circulars or other documents,
+						much of the guidance in this subtitle existed prior to the establishment of <a
+							href="/on/2024-03-01/title-2" class="cfr external">title 2 of the CFR.</a> Specifically:
+					</p>
+					<div class="table-wrapper">
+						<table>
+							<thead>
+								<tr>
+									<th>Guidance
+										<br>in * * *
+</th>
+									<th>On
+										<br>* * *
+</th>
+									<th>Previously
+										<br>was in * * *
+</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="align-left">(a) Chapter I, part 180</td>
+									<td class="align-left">Nonprocurement debarment and suspension</td>
+									<td class="align-left">OMB guidance that conforms with the government-wide common
+										rule (see <a href="https://www.federalregister.gov/citation/60-FR-33036"
+											class="fr-reference" data-reference="60 FR 33036">60 FR 33036</a>, June 26,
+										1995).
+									</td>
+								</tr>
+								<tr>
+									<td class="align-left">(b) Chapter I, part 182</td>
+									<td class="align-left">Drug-free workplace requirements</td>
+									<td class="align-left">OMB guidance (<a
+											href="https://www.federalregister.gov/citation/54-FR-4946"
+											class="fr-reference" data-reference="54 FR 4946">54 FR 4946</a>, January 31,
+										1989) and a government-wide common rule (as amended at <a
+											href="https://www.federalregister.gov/citation/68-FR-66534"
+											class="fr-reference" data-reference="68 FR 66534">68 FR 66534</a>, November
+										26, 2003).
+									</td>
+								</tr>
+								<tr>
+									<td class="align-left">(c) Chapter II, part 200</td>
+									<td class="align-left">Uniform administrative requirements, cost principles, and
+										audit requirements for federal awards</td>
+									<td class="align-left">OMB Circulars A-21, “Cost Principles for Educational
+										Institutions” (Chapter II, part 225); A-87, “Cost Principles for State, Local
+										and Indian Tribal Governments” (Chapter II, part 225); A-89, “Federal Domestic
+										Assistance Program Information”; ”; A-102 and a government-wide common rule (<a
+											href="https://www.federalregister.gov/citation/53-FR-8034"
+											class="fr-reference" data-reference="53 FR 8034">53 FR 8034</a>, March 11,
+										1988); A-110, “Uniform Administrative Requirements for Awards and Other
+										Agreements with Institutions of Higher Education, Hospitals, and Other Nonprofit
+										Organizations” (Chapter II, part 215); A-122, “Cost Principles for Non-Profit
+										Organizations” (Chapter II, part 230); and A-133 “Audits of States, Local
+										Governments and Non-Profit Organizations”.</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<p class="citation">[<a href="https://www.federalregister.gov/citation/79-FR-75878"
+							class="fr-reference" data-reference="79 FR 75878">79 FR 75878</a>, Dec. 19, 2014]
+					</p>
+				</div>
+
+				<div class="section" id="1.220">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.220","citation":"2 CFR 1.220"}'>
+						§ 1.220 Federal agency implementation of this subtitle.</h4>
+					<p>A Federal agency that awards grants and agreements subject to the guidance in this subtitle
+						implements the guidance in agency regulations in <a href="/on/2024-03-01/title-2/subtitle-B"
+							class="cfr external">subtitle B of this title</a> and/or in policy and procedural issuances,
+						such as internal instructions to the agency's awarding and administering officials. An applicant
+						or recipient would see the effect of that implementation in the organization and content of the
+						agency's announcements of funding opportunities and in its award terms and conditions. </p>
+				</div>
+
+				<div class="section" id="1.230">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.230","citation":"2 CFR 1.230"}'>
+						§ 1.230 Maintenance of this subtitle.</h4>
+					<p>OMB issues guidance in this subtitle after publication in the
+						<span class="minor-caps">Federal Register.</span> Any portion of the guidance that has a
+						potential impact on the public is published with an opportunity for public comment. </p>
+				</div>
+
+			</div>
+
+			<div class="subpart" id="subpart-C">
+				<h2
+					data-hierarchy-metadata="{&quot;path&quot;:&quot;/on/2024-03-01/title-2/part-1/subpart-C&quot;,&quot;citation&quot;:&quot;2 CFR Part 1 Subpart C&quot;}">
+					Subpart C—Responsibilities of OMB and Federal Agencies</h2>
+				<div class="section" id="1.300">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.300","citation":"2 CFR 1.300"}'>
+						§ 1.300 OMB responsibilities.</h4>
+					<p>OMB is responsible for: </p>
+					<div id="p-1.300(a)">
+						<p class="indent-1" data-title="1.300(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							Issuing and maintaining the guidance in this subtitle, as described in <a
+								href="/on/2024-03-01/title-2/section-1.230" class="cfr external">§ 1.230</a>. </p>
+					</div>
+					<div id="p-1.300(b)">
+						<p class="indent-1" data-title="1.300(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							Interpreting the policy requirements in this subtitle. </p>
+					</div>
+					<div id="p-1.300(c)">
+						<p class="indent-1" data-title="1.300(c)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>c<span class="paren">)</span></span>
+							Reviewing Federal agency regulations implementing the requirements of this subtitle, as
+							required by Executive Order 12866. </p>
+					</div>
+					<div id="p-1.300(d)">
+						<p class="indent-1" data-title="1.300(d)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>d<span class="paren">)</span></span>
+							Conducting broad oversight of government-wide compliance with the guidance in this subtitle.
+						</p>
+					</div>
+					<div id="p-1.300(e)">
+						<p class="indent-1" data-title="1.300(e)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>e<span class="paren">)</span></span>
+							Performing other OMB functions specified in this subtitle. </p>
+					</div>
+				</div>
+
+				<div class="section" id="1.305">
+					<h4
+						data-hierarchy-metadata='{"path":"/on/2024-03-01/title-2/section-1.305","citation":"2 CFR 1.305"}'>
+						§ 1.305 Federal agency responsibilities.</h4>
+					<p>The head of each Federal agency that awards and administers grants and agreements subject to the
+						guidance in this subtitle is responsible for: </p>
+					<div id="p-1.305(a)">
+						<p class="indent-1" data-title="1.305(a)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>a<span class="paren">)</span></span>
+							Implementing the guidance in this subtitle. </p>
+					</div>
+					<div id="p-1.305(b)">
+						<p class="indent-1" data-title="1.305(b)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>b<span class="paren">)</span></span>
+							Ensuring that the agency's components and subcomponents comply with the agency's
+							implementation of the guidance. </p>
+					</div>
+					<div id="p-1.305(c)">
+						<p class="indent-1" data-title="1.305(c)">
+							<span class="paragraph-hierarchy"><span class="paren">(</span>c<span class="paren">)</span></span>
+							Performing other functions specified in this subtitle. </p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+    </div>
+</div>
+`;
+
+export default htmlString;
--- server.js
+++ server.js
@@ -1,5 +1,86 @@
 import getDocument from "./routes/documentRoute.js"
+import sax from "sax";
+import htmlString from "./example.js";
 
-const getHTML = getDocument().then(response=> {
-    console.log(response);
-})
+const parser = sax.parser(false, {
+    normalize: true
+});
+
+const getHTML = getDocument().then(response => {
+    //console.log(response);
+
+    try {
+        let parsedHTML = {};
+        let elementName = null;
+        let lastID = null;
+        let lastTitle = null;
+        let lastSubtitle = null;
+        let lastPart = null;
+        let lastSubpart = null;
+        let lastSection = null;
+        let lastDiv = null;
+
+        parser.onopentag = (node) => {
+            elementName = node.name;
+
+
+            if (node.name === 'DIV' && node.attributes.ID !== undefined) {
+                lastID = node.attributes.ID;
+                lastDiv = node.attributes.CLASS;
+                if (node.attributes.CLASS === 'title') lastTitle = lastID;
+                if (node.attributes.CLASS === 'subtitle') lastSubtitle = lastID;
+                if (node.attributes.CLASS === 'part') lastPart = lastID;
+                if (node.attributes.CLASS === 'subpart') lastSubpart = lastID;
+                if (node.attributes.CLASS === 'section') lastSection = lastID;
+            }
+        }
+
+        // parser.onattribute = (attr) => {
+        //     console.log(attr);
+        // }
+
+        parser.ontext = (text) => {
+
+            const trimmedText = text.trim();
+            if (trimmedText !== "") {
+                if (elementName === 'H1' || elementName === 'H2' || elementName === 'H3' || elementName === 'H4' || elementName === 'H5' || elementName === 'H6' || lastDiv !== null) {
+                    let obj = { title: text };
+                    if (lastDiv === 'title') {
+                        if (parsedHTML[lastTitle] === undefined) parsedHTML[lastTitle] = obj;
+                        else parsedHTML[lastTitle].title += text;
+                    }
+                    else if (lastDiv === 'subtitle') {
+                        if (parsedHTML[lastTitle][lastSubtitle] === undefined) parsedHTML[lastTitle][lastSubtitle] = obj;
+                        else parsedHTML[lastTitle][lastSubtitle].title += text;
+                    }
+                    else if (lastDiv === 'part') {
+                        if (parsedHTML[lastTitle][lastSubtitle][lastPart] === undefined) parsedHTML[lastTitle][lastSubtitle][lastPart] = obj;
+                        else parsedHTML[lastTitle][lastSubtitle][lastPart].title += text;
+                    }
+                    else if (lastDiv === 'subpart') {
+                        if (parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart] === undefined) parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart] = obj;
+                        else parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart].title += text;
+                    }
+                    else if (lastDiv === 'section') {
+                        if (parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart][lastSection] === undefined) parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart][lastSection] = obj;
+                        else parsedHTML[lastTitle][lastSubtitle][lastPart][lastSubpart][lastSection].title += text;
+                    }
+                }
+            }
+        }
+
+        parser.onclosetag = (node) => {
+            if (node === 'H1' || node === 'H2' || node === 'H3' || node === 'H4' || node === 'H5' || node === 'H6') {
+                lastDiv = null;
+            }
+        }
+        parser.onend = () => {
+            console.log(JSON.stringify(parsedHTML, null, 2));
+        }
+
+        parser.write(htmlString).close();
+    } catch (error) {
+        console.log(error);
+    }
+
+});
